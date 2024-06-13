b745e01b273266b34743bf1c80c63fcd8d902432
zhangcheng <zh.milo@gmail.com>
2024-06-13 10:41:21 +0800

    add vercel

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
-from binace_api import Binance
 import json
 import math
 import datetime
 import random
 import requests
+
+from binance.client import Client
+
+
+class Binance:
+    def __init__(self, api_key, api_secret):
+        self.client = Client(api_key, api_secret)
+
+    def withdraw(self, coin, address, amount, network):
+        return self.client.withdraw(
+            coin=coin,
+            address=address,
+            amount=amount,
+            network=network,
+        )
+
+    def all_coins(self):
+        return self.client.get_all_coins_info()
 
 
 def get_ip():
